8e1d418b00f6e99ecb7f23d705a9b80993b76d1d
Wren <otherwisejunk@gmail.com>
2023-06-18 18:45:41 -0400

    add magic goolsball command

--- Handlers/NewStatusHandlers.ts
+++ Handlers/NewStatusHandlers.ts
@@ -1,7 +1,8 @@
 import { mastodon } from "masto";
-import { RandomizationUtilities as Random } from "../Utilities/RandomizationUtilities.ts";
+import { RandomizationUtilities as Random, RandomizationUtilities } from "../Utilities/RandomizationUtilities.ts";
 import { MastodonUtilities } from "../Utilities/Mastodon.Utilities.ts";
 import { SecretMalarkeyLevel, MalarkeyLevels } from "../Constants/MalarkeyLevel.constants.ts";
+import fs from "node:fs";
 
 export class NewStatusHandlers {
   static async MalarkeyLevel(status: mastodon.v1.Status, mastodonUtilities: MastodonUtilities) {
@@ -10,6 +11,15 @@ export class NewStatusHandlers {
 
       let attachment = await mastodonUtilities.createNewMediaAttachmentFromPath(malarkeyLevel[1], malarkeyLevel[2]);
       mastodonUtilities.createNewStatus(malarkeyLevel[0], [attachment.id], status.id);
+    }
+  }
+  static async MagicGoolsball(status: mastodon.v1.Status, mastodonUtilities: MastodonUtilities) {
+    if (this.StatusContainsMagicGoolsballCommand(status)) {
+      let pathToGoolsball = "./Media/MagicGoolsball/";
+      let selectedGoolsballPath = `${pathToGoolsball}${RandomizationUtilities.sample(fs.readdirSync(pathToGoolsball))}`;
+
+      let attachment = await mastodonUtilities.createNewMediaAttachmentFromPath(selectedGoolsballPath, "Oh that Austan Goolsbee, what a rascal");
+      mastodonUtilities.createNewStatus("You shake the Magic Goolsball(tm) and...", [attachment.id], status.id);
     }
   }
   static StatusContainsMalarkeyLevelCommand(status: mastodon.v1.Status) {
